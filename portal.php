085a0a8175e96924c1ad73711d8f2924b8e1cd23
fflam <fflameq@gmail.com>
2024-09-28 08:16:42 +0000

    first commit

---
+++ portal.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'name' => 'Pay 2 Win Gaming',
+    'image' => 'https://games.pay2wingaming.com/Images/Asset_2Game_BOY_2.png',
+    'version' => '1.0.0',
+    'wemx_version' => '1.7.1',
+    'author' => 'Pay2Win',
+
+    // Below are assets for the theme
+    'folder' => 'pay2win',
+    'assets' => '/assets/themes/default/'
+
+];
